f57426024a122252852d21576eff4309f65d24e5
mojojojo20 <menonk484@gmail.com>
2020-08-30 19:11:12 +0530

    add form

---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const ContactForm = () => {
+
+    return (
+            <form className="w-full max-w-lg">
+                <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+                        First Name
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
+                    {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
+                    </div>
+                    <div className="w-full md:w-1/2 px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
+                        Last Name
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+                    </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-2">
+                    <div className="w-full px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-city">
+                        Message
+                    </label>
+                    <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Type your message here" />
+                    </div>               
+                </div>
+                <button className="shadow mt-2 bg-gray-400 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-black-100 hover:text-white font-bold py-2 px-4 rounded" type="button">
+                            Sign Up
+                </button>     
+            </form>
+    )
+}
+
+export default ContactForm;
